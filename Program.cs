e498eae671a9bd3a3fc04b9252cf52581f89bbf0
Valentisa11 <valya.kobylina@yandex.ru>
2023-01-24 15:25:20 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Task13_6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = Stopwatch.StartNew();
+            var strings = new List<string>();
+            string text = File.ReadAllText("D:\\Downloads\\Text1.txt");
+            strings.Add(text);
+            Console.WriteLine($"List Time Elapsed: {stopWatch.Elapsed.TotalMilliseconds} ms");//замер для List 
+
+            stopWatch = Stopwatch.StartNew();
+            LinkedList<string> LinkedList = new LinkedList<string>();
+            text = File.ReadAllText("D:\\Downloads\\Text1.txt");
+            LinkedList.AddFirst(text);
+            Console.WriteLine($"LinkedList Time Elapsed: {stopWatch.Elapsed.TotalMilliseconds} ms");//замер для  LinkedList. Оказался быстрее
+        }
+    }
+}
